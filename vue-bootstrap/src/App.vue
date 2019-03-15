b9f325da5f06d51b9cd5dc1bf97d3db5d130abeb
sosohui <gunbbangmom@gmail.com>
2019-03-15 18:59:56 +0900

    second commit

---
+++ vue-bootstrap/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia facere reprehenderit, consequatur dolorum id quisquam quidem animi rerum amet assumenda?
+        </b-col>
+        <b-col cols=6>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed consequatur, excepturi placeat hic rem laborum perferendis sunt recusandae eaque praesentium.
+        </b-col>
+        <b-col>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident autem facere mollitia natus debitis iste, rem quisquam error distinctio. Inventore ex laudantium consequatur autem voluptatem. Minus nostrum mollitia accusantium at.
+        </b-col>
+      </b-row>
+    </b-container>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam laborum repellat dolores eveniet! Eius dolorem fugiat assumenda facilis nisi molestias!
+        </div>
+        <div class="col">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique laborum hic eos fugiat reprehenderit voluptate veritatis adipisci ut possimus natus.
+        </div>
+        <div class="col">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dolorum vero in quidem distinctio beatae eveniet tempora quae. Quos, deserunt.
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+</style>
