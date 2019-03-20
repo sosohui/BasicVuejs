5af7e96fe76fbb97a4d89b8b189623a91cdcbecc
sosohui <gunbbangmom@gmail.com>
2019-03-20 10:57:16 +0900

    commit fourth

---
+++ vuex/src/components/Users/AllUsers.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>All Users({{ count }})</h1>
+    <h3>SeoulUsers : {{ seouls }}({{ percent }}%)</h3>
+
+    <v-list two-line>
+      <v-list-tile
+        v-for="(user, index) in users"
+        :key="index"
+        avatar
+      >
+        <v-list-tile-avatar color="grey lighten-3">
+          <img :src="user.src">
+        </v-list-tile-avatar>
+
+        <v-list-tile-content>
+          <v-list-tile-title v-html="user.name"></v-list-tile-title>
+          <v-list-tile-sub-title>id:#{{index}} / {{user.address}} 거주</v-list-tile-sub-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+
+  </div>
+</template>
+
+<script>
+import { EventBus } from '@/main.js'
+import { mapState,mapGetters } from 'vuex'
+
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    computed: {
+      // ...mapGetters(['allUsersCount','countOfSeoul','percentOfSeoul'])
+      ...mapGetters({
+        count : 'allUsersCount',
+        seouls: 'countOfSeoul',
+        percent: 'percentOfSeoul'
+      }),
+      ...mapState({
+        users: 'allUsers'
+      })
+    },
+    mounted() {
+      EventBus.$on('signUp', users => {
+        this.$store.state.allUsers.push(users)
+      })
+    }
+  }
+</script>
